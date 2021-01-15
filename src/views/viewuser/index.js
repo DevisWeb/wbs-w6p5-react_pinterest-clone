cd9994d0f5764eb42426971f2e18419a7bdf8432
Hermann Marx <marxhermann@web.de>
2021-01-15 15:30:00 +0100

    Added const avater, userName, userId, posts,uerPosts, url. Added axios get request. Prepared  userPosts as an array with the user specific posts. Added img, p, PostGrid to the HTML

--- src/views/viewuser/index.js
+++ src/views/viewuser/index.js
@@ -1,4 +1,5 @@
 import { unstable_concurrentAct } from "react-dom/cjs/react-dom-test-utils.development";
+import "../../../.env"
 
 export default function ViewUser({user}) {
 
@@ -7,11 +8,11 @@ export default function ViewUser({user}) {
     const userId = {user.fields.user.sys.id}
     const posts = []
     const userPosts = []
+    const url = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}`
 
     const getUserPosts = () => {
         axios
-      .get(
-        "https://cdn.contentful.com/spaces/wwsh9kp1nfrc/environments/master/entries?access_token=6JzF_q1CD1Htat5n-3DjPBwGuw5B1lbevCKfiqogqX4")
+      .get(url)
       .then((res) => {
         console.log(res.data.items);
         res.data.items.map((iteration, index) => {
