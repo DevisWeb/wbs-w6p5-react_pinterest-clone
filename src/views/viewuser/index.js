ff953d8103c29ccb525c03cd9210ed21b67fe3cd
Hermann Marx <marxhermann@web.de>
2021-01-15 17:18:03 +0100

    test

--- src/views/viewuser/index.js
+++ src/views/viewuser/index.js
@@ -1,11 +1,14 @@
 import { unstable_concurrentAct } from "react-dom/cjs/react-dom-test-utils.development";
-import "../../../.env"
+import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
 
-export default function ViewUser({user}) {
-
-    const avatar = {user.fields.avatarLink}
-    const userName = {user.fields.name}
-    const userId = {user.fields.user.sys.id}
+export default function ViewUser() {
+  //Prop = name
+  //Looking for a specific user: https://cdn.contentful.com/spaces/wwsh9kp1nfrc/environments/master/entries?access_token=6JzF_q1CD1Htat5n-3DjPBwGuw5B1lbevCKfiqogqX4&content_type=post&fields.user.sys.id=4WpLfOWGSLrrKs8jPLw3yt
+  /*     const { id } = useParams();
+    const avatar = user.fields.avatarLink
+    const userName = user.fields.name
+    const userId = user.fields.user.sys.id
     const posts = []
     const userPosts = []
     const url = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}`
@@ -29,9 +32,13 @@ export default function ViewUser({user}) {
   };
     }
 
-    return <div>
-        <img src={avatar} alt="avatar"/>
+    useEffect(()=> getUserPosts(),[]) */
+
+  return (
+    <div>
+      {/* <img src={avatar} alt="avatar"/>
         <p>{userName}</p>
-        <PostGrid userPosts={userPosts}/>
-        </div>
+        <PostGrid userPosts={userPosts}/> */}
+    </div>
+  );
 }
