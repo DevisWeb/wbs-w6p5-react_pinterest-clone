6b58fdc7264e26bcfa6af63421079b069c1bb189
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-01-15 18:26:15 +0100

    set up ViewPost with basic functionality

--- src/views/viewpost/index.js
+++ src/views/viewpost/index.js
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 export default function ViewPost() {
-    return <div>I am a ViewPost</div>
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://cdn.contentful.com/spaces/wwsh9kp1nfrc/environments/master/entries?access_token=6JzF_q1CD1Htat5n-3DjPBwGuw5B1lbevCKfiqogqX4&content_type=post&sys.id=" +
+          id
+      )
+      .then((response) => {
+        setPost(response.data.items[0].fields);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
+  
+  return <div>{JSON.stringify(post)}</div>;
 }
