3e157701bc9d33c9c0a04f9567dd157e20474ef7
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-01-15 10:14:42 +0100

    IMPL: initial SearchBar Component

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,3 +1,32 @@
-export default SearchBar = () => {
-    return <div>I am a SearchBar</div>
+import { useState } from 'react'
+import { Link, useHistory, withRouter } from 'react-router-dom'
+import './styles.css'
+
+function SearchBar() {
+  const [value, setValue] = useState('')
+  const history = useHistory()
+  return (
+    <div className="searchbar">
+      <form
+        className="searchbar-form"
+        onSubmit={(ev) => {
+          ev.preventDefault()
+
+          value ? history.push('search=' + value) : history.push('/')
+        }}
+      >
+        <input
+          placeholder="i am searching for"
+          className="searchbar-input"
+          value={value}
+          onChange={(ev) => setValue(ev.target.value)}
+        ></input>
+        <button className="searchbar-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  )
 }
+
+export default withRouter(SearchBar)
