42b9d403036b1f5496bf3987e81c7a69fc1664b9
Hermann Marx <marxhermann@web.de>
2021-01-15 14:49:26 +0100

    basic code ViewUser

--- src/views/viewuser/index.js
+++ src/views/viewuser/index.js
@@ -1,3 +1,36 @@
-export default function ViewUser() {
-    return <div>I am a ViewUser</div>
+import { unstable_concurrentAct } from "react-dom/cjs/react-dom-test-utils.development";
+
+export default function ViewUser({user}) {
+
+    const avatar = {user.fields.avatarLink}
+    const userName = {user.fields.name}
+    const userId = {user.fields.user.sys.id}
+    const posts = []
+    const userPosts = []
+
+    const getUserPosts = () => {
+        axios
+      .get(
+        "https://cdn.contentful.com/spaces/wwsh9kp1nfrc/environments/master/entries?access_token=6JzF_q1CD1Htat5n-3DjPBwGuw5B1lbevCKfiqogqX4")
+      .then((res) => {
+        console.log(res.data.items);
+        res.data.items.map((iteration, index) => {
+          if (iteration.fields.title !== undefined) posts.push(iteration);
+        });
+        posts.map((iteration, index) => {
+          if (iteration.fields.user.sys.id === userId)
+            userPosts.push(iteration);
+        });
+        console.log(posts);
+        console.log(userPosts);
+      })
+      .catch((er) => console.log(er));
+  };
+    }
+
+    return <div>
+        <img src={avatar} alt="avatar"/>
+        <p>{userName}</p>
+        <PostGrid userPosts={userPosts}/>
+        </div>
 }
