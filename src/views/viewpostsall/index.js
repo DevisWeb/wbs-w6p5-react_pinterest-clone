7db18bc6ac7ea8bfdf32e6d5cb15488eb70a3c0a
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-18 10:39:59 +0100

    Import useState and axios; add callApi function which sets all posts

--- src/views/viewpostsall/index.js
+++ src/views/viewpostsall/index.js
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function ViewPostsAll() {
+  const [postsAll, setPostsAll] = useState([]);
   let { search } = useParams();
 
   const urlFetchAllPosts = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post`;
@@ -13,5 +16,15 @@ export default function ViewPostsAll() {
   const findEndpoint = () => {
     return search === "posts" ? urlFetchAllPosts : urlFetchSearchResults;
   };
+
+  const callApi = (endpoint) => {
+    axios
+      .get(endpoint)
+      .then((response) => {
+        setPostsAll(response.data.items);
+      })
+      .catch((error) => console.error(error));
+  };
+
   return <div>I am a ViewPostsAll</div>;
 }
