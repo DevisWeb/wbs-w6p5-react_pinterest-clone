b5c27718a5ee7a662fd06cfc97693eefcea0327f
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-18 22:08:33 +0100

    Add masonry and set breakpoints

--- src/components/postgrid/index.js
+++ src/components/postgrid/index.js
@@ -1,12 +1,32 @@
 // Import components
 import PostCard from "../postcard/";
 
+// Import styles
+import "./styles.css";
+import Masonry from "react-masonry-css";
+
 export default function PostGrid({ postsAll }) {
+  // Set breakpoints
+  const breakpointColumnsObj = {
+    default: 4,
+    1000: 3,
+    500: 2,
+    350: 1,
+  };
   return (
-    <div>
-      {postsAll.map((post, index) => {
-        return <PostCard key={index} post={post} />;
-      })}
+    <div className="post-grid-stack">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="post-grid-masonry"
+        columnClassName="post-grid-column"
+      >
+        {postsAll
+          // Shuffle array before iteration
+          .sort(() => Math.random() - 0.5)
+          .map((post, index) => {
+            return <PostCard key={index} post={post} />;
+          })}
+      </Masonry>
     </div>
   );
 }
