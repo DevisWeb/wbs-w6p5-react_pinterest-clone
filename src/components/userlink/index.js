9197290d58aaf9579d6a53baccbf7c3f7f623824
Hermann Marx <marxhermann@web.de>
2021-01-18 22:12:52 +0100

    Added avatar + username. Added link to userView. Created styles.css and added basic style to the component.

--- src/components/userlink/index.js
+++ src/components/userlink/index.js
@@ -1,3 +1,17 @@
+import "./styles.css";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 export default function UserLink({ user }) {
-  return <div className="user-link">I am Userlink: {JSON.stringify(user)}</div>;
+  useEffect(() => console.log(user));
+  return (
+    <div>
+      <Link to={`/users/${user.name}`}>
+        <div className="user-link">
+          <img src={user.avatarLink} className="img-user-link" />
+          {user.name}
+        </div>
+      </Link>
+    </div>
+  );
 }
