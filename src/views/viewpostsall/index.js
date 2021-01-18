57cc3cd4c6387645eeb80a9f49fd61830f0ae324
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-18 17:05:01 +0100

    Fix search

--- src/views/viewpostsall/index.js
+++ src/views/viewpostsall/index.js
@@ -12,11 +12,7 @@ export default function ViewPostsAll() {
 
   // Define API endpoints
   const urlFetchAllPosts = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post`;
-  const urlFetchSearchResults = `${
-    process.env.REACT_APP_API_ENDPOINT
-  }?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&query=${
-    search?.split("=")[1]
-  }`;
+  const urlFetchSearchResults = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&query=${search}`;
 
   const findEndpoint = () => {
     return search === undefined ? urlFetchAllPosts : urlFetchSearchResults;
