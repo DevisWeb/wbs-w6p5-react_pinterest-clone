964c55bf281356c5b0aa1d178646f0ecf9490375
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-01-18 12:48:03 +0100

    Add a key to list of posts | Import & render 'Rating' component
    Further adjustments:
    - change label to htmlFor for consistency with the DOM property API
    - pass props to Rating component
    - adjust code structure of list

--- src/views/viewpostsbestrated/index.js
+++ src/views/viewpostsbestrated/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 // import PostCard from "../../components/postcard";
+import Rating from "../../components/rating";
 
 export default function ViewPostsBestRated() {
   const [rating, setRating] = useState(null);
@@ -35,7 +36,8 @@ export default function ViewPostsBestRated() {
     <div>
       <h3>Filter Posts by rating *****</h3>
       <div>
-        <label for="rating">Show all posts rated with: </label>
+        {/* <label htmlFor="rating">Show all posts rated with: </label> */}
+        <label htmlFor="rating">Show all posts rated with: </label>
 
         <select onChange={searchValue} name="rating" id="rating">
           <option value="5">5 *****</option>
@@ -47,48 +49,53 @@ export default function ViewPostsBestRated() {
         {/* <input onChange={searchValue} type="text" /> */}
         <button onClick={getPostsByRating}>Search</button>
       </div>
-      {rating &&
-        rating.map((selectedPosts) => (
-          <div>
-            {
-              <div>
-                <br />
-                <h3>{selectedPosts.fields.title}</h3>
-                <img
-                  src={`${selectedPosts.fields.imageLink}`}
-                  alt={`Link: ${selectedPosts.fields.imageLink}`}
-                />
-                <p>{selectedPosts.fields.description}</p>
-                <p>Rating: {selectedPosts.fields.rating}</p>
-                <span>User-ID: {selectedPosts.fields.user.sys.id}</span>
-              </div>
-            }
-            {/* <PostCard
-              key={selectedPosts.fields.user.sys.id}
+      <ul>
+        {/* {rating &&
+          rating.map((selectedPosts) => (
+            <PostCard
+              key={selectedPosts.sys.id}
               id={selectedPosts.fields.user.sys.id}
               title={selectedPosts.fields.title}
               description={selectedPosts.fields.description}
               rating={selectedPosts.fields.rating}
               src={`${selectedPosts.fields.imageLink}`}
               alt={`Link: ${selectedPosts.fields.imageLink}`}
-            /> */}
-          </div>
-        ))}
+            />
+          ))} */}
+        {rating &&
+          rating.map((selectedPosts) => (
+            <li key={selectedPosts.sys.id}>
+              <h3>{selectedPosts.fields.title}</h3>
+              <img
+                src={`${selectedPosts.fields.imageLink}`}
+                alt={`Link: ${selectedPosts.fields.imageLink}`}
+              />
+              <p>{selectedPosts.fields.description}</p>
+
+              {/* <p>Rating: {selectedPosts.fields.rating}</p> */}
+              <Rating rating={selectedPosts.fields.rating} />
+
+              <span>User-ID: {selectedPosts.fields.user.sys.id}</span>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
 
 // Test-Code for PostCard component:
 
+// import Rating from "../../components/rating";
+
 // export default function PostCard({ title, description, src, rating, id }) {
-//     return (
-//       <div>
-//         <br />
-//         <h3>{title}</h3>
-//         <img src={src} alt={title} />
-//         <p>{description}</p>
-//         <p>{rating}</p>
-//         <span>{id}</span>
-//       </div>
-//     );
-//   }
+//   return (
+//     <div>
+//       <h3>{title}</h3>
+//       <img src={src} alt={title} />
+//       <p>{description}</p>
+//       {/* <p>{rating}</p> */}
+//       <Rating rating={rating} />
+//       <span>{id}</span>
+//     </div>
+//   );
+// }
