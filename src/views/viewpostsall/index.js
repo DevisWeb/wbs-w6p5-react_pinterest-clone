43ae6c9f937d1f029a035b2c8742986102ab69c8
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-18 10:37:46 +0100

    Get search keyword from params and define endpoints

--- src/views/viewpostsall/index.js
+++ src/views/viewpostsall/index.js
@@ -1,3 +1,13 @@
+import { useParams } from "react-router-dom";
+
 export default function ViewPostsAll() {
-    return <div>I am a ViewPostsAll</div>
+  let { search } = useParams();
+
+  const urlFetchAllPosts = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post`;
+  const urlFetchSearchResults = `${
+    process.env.REACT_APP_API_ENDPOINT
+  }?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&query=${
+    search?.split("=")[1]
+  }`;
+  return <div>I am a ViewPostsAll</div>;
 }
