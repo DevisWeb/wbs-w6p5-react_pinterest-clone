80fddbbbe9eb1ff33720813dd26ee530c3addcdd
Hermann Marx <marxhermann@web.de>
2021-01-18 17:23:26 +0100

    Working versions with commnets. Could be stil cleansed.

--- src/views/viewuser/index.js
+++ src/views/viewuser/index.js
@@ -1,18 +1,18 @@
-import { unstable_concurrentAct } from "react-dom/cjs/react-dom-test-utils.development";
 import { useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PostGrid from "../../components/postgrid";
 
-export default function ViewUser({ userName = "Pikachu" }) {
+export default function ViewUser() {
   // name the username that will be passed as a prop. Set by default on "Pikachu" for testing
-  const name = userName;
+  let { id } = useParams();
+  const name = id;
   // the state variable for the avatarLink used as the profile pic of the user
-  let [avatar, setAvatar] = useState();
+  const [avatar, setAvatar] = useState();
   // the state variable of the userId will be used to get the posts of the specific user. Used in the second axios request.
-  let [userId, setUserId] = useState();
+  const [userId, setUserId] = useState();
   // userPosts will be set to an array with all the posts of the specific user. Will be set in the second axios request.
-  let [userPosts, setUserPosts] = useState();
+  const [userPosts, setUserPosts] = useState([]);
 
   const getUsersPosts = () => {
     axios
@@ -29,42 +29,39 @@ export default function ViewUser({ userName = "Pikachu" }) {
         // here the unique userId is set which will be reused to find his specific posts
         setUserId(res.data.items[0].sys.id);
         //Although the variable avatar can be displayed in the HTML, here it is still not reset. What is the reason?
-        console.log("This is avatar link: ", avatar);
+        //console.log("This is avatar link: ", avatar);
       })
-      .then(
-        axios
-          .get(
-            //ISSUE TO BE FIXED: This request gets all the posts of the user, but it still doesn't can take the variable userId somehow. For testing the Id of Pikachu is used.
-            `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&fields.user.sys.id=6YUqfiLXoA1cDLboNql1oF`
-          )
-          .then((res) => {
-            // Setting the variable userPosts equal the request which has all the posts of this user.
-            setUserPosts(res);
-            // here you can check that all user's post are in the response.
-            console.log("All user's posts", res);
-            // ISSUE TO BE FIXED: somehow the variable userPosts is here not set already. Check it in the console.
-            //But it works in the HTML.
-            console.log("UserPosts =", userPosts);
-            //Another testing of the response.
-            console.log(res.data.items[0].fields.title);
-          })
-      )
       .catch((error) => console.error(error));
   };
 
-  useState(() => getUsersPosts(), []);
+  const getUserPosts2 = () => {
+    axios
+      .get(
+        //ISSUE TO BE FIXED: This request gets all the posts of the user, but it still doesn't can take the variable userId somehow. For testing the Id of Pikachu is used.
+        `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&fields.user.sys.id=${userId}`
+      )
+      .then((res) => {
+        // Setting the variable userPosts equal the request which has all the posts of this user.
+        setUserPosts(res.data.items);
+        // here you can check that all user's post are in the response.
+        //console.log("All user's posts", res.data.items);
+        // ISSUE TO BE FIXED: somehow the variable userPosts is here not set already. Check it in the console.
+        //But it works in the HTML.
+        //console.log("UserPosts =", userPosts);
+        //Another testing of the response.
+        // console.log(res.data.items[0].fields.title);
+      });
+  };
+
+  useEffect(() => getUsersPosts(), []);
+  useEffect(() => getUserPosts2(), [userId]);
 
   return (
     <div>
       <img src={avatar} alt={name} />
       <p>{name}</p>
-      <PostGrid posts={userPosts} />
-      <div>
-        <br />
-        This is userId: {userId}
-        <br />
-        {/*  This is UserPosts: {userPosts.data.items[0].fields.title} */}
-      </div>
+      <PostGrid postsAll={userPosts} />
+      <div></div>
     </div>
   );
 }
