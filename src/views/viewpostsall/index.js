622c3a6c0afcdaa4cf54ac885a6117753850da62
Jasmin Jenull <jasmin.jenull@gmx.at>
2021-01-18 10:44:51 +0100

    Add comments

--- src/views/viewpostsall/index.js
+++ src/views/viewpostsall/index.js
@@ -1,13 +1,16 @@
+// Import react modules
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+//Import components
 import PostGrid from "../../components/postgrid/";
 
 export default function ViewPostsAll() {
   const [postsAll, setPostsAll] = useState([]);
   let { search } = useParams();
 
+  // Define API endpoints
   const urlFetchAllPosts = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post`;
   const urlFetchSearchResults = `${
     process.env.REACT_APP_API_ENDPOINT
@@ -28,6 +31,7 @@ export default function ViewPostsAll() {
       .catch((error) => console.error(error));
   };
 
+  // Invoke callApi function on search param update
   useEffect(() => {
     callApi(findEndpoint());
   }, [search]);
