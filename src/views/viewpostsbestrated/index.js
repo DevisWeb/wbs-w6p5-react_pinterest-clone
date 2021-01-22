dc87debc97e64b4285d56436cbb66b527d2e735c
Devis <22038252+DevisWeb@users.noreply.github.com>
2021-01-22 12:37:37 +0100

    Adjust Styling for select manu & Render Rating component
    + adjust events + functions for select menu

--- src/views/viewpostsbestrated/index.js
+++ src/views/viewpostsbestrated/index.js
@@ -1,11 +1,14 @@
+import "./styles.css";
 import { useEffect, useState } from "react";
 import { Fragment } from "react";
 import axios from "axios";
 import PostGrid from "../../components/postgrid/";
+import Rating from "../../components/rating/";
 
 export default function ViewPostsBestRated() {
   const [ratedPosts, setRatedPosts] = useState([]);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState("5");
+
   const query = `${process.env.REACT_APP_API_ENDPOINT}?access_token=${process.env.REACT_APP_API_KEY}&content_type=post&fields.rating`;
 
   useEffect(() => {
@@ -19,9 +22,15 @@ export default function ViewPostsBestRated() {
       .catch((err) => console.log(err));
   }, []);
 
-  // Event
-  const searchValue = (e) => {
-    setInput(e.target.value); //console.log(e.target.value);
+  // Events
+
+  const onMouseEnter = (e) => {
+    console.log(e.target.value);
+  };
+
+  const getValue = (e) => {
+    setInput(e.target.value);
+    console.log(e.target.value);
   };
 
   const getPostsByRating = (e) => {
@@ -36,19 +45,29 @@ export default function ViewPostsBestRated() {
 
   return (
     <Fragment>
-      <h3>Filter Posts by rating *****</h3>
-      <p>
-        <label htmlFor="rating">Show all posts rated with: </label>
-        <select onChange={searchValue} name="rating" id="rating">
-          <option value="5">5 *****</option>
-          <option value="4">4 ****</option>
-          <option value="3">3 ***</option>
-          <option value="2">2 **</option>
-          <option value="1">1 *</option>
-        </select>
-        {/* <input onChange={searchValue} type="text" /> */}
-        <button onClick={getPostsByRating}>Search</button>
-      </p>
+      <div className="vp-byrating-wrapper">
+        <h3 class="uppercase">Top rated posts</h3>
+        <div className="vp-byrating-section flex">
+          <label htmlFor="rating">Show all posts rated with:</label>
+          <select
+            className="cursor-pointer vp-byrating-select"
+            onMouseEnter={onMouseEnter}
+            onChange={getValue}
+            onMouseLeave={getPostsByRating}
+            name="rating"
+            id="rating"
+          >
+            <option value="5" selected>
+              5
+            </option>
+            <option value="4">4</option>
+            <option value="3">3</option>
+            <option value="2">2</option>
+            <option value="1">1</option>
+          </select>
+          <Rating className="starhover" rating={input} />
+        </div>
+      </div>
       <PostGrid postsAll={ratedPosts} />
     </Fragment>
   );
